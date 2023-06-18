0d339e054958a384636b4c1876619b5bd9b31866
FAHIM-MARUF <106343265+FAHIM-MARUF@users.noreply.github.com>
2023-06-19 00:23:13 +0600

    FAHIM

---
+++ nput.py
@@ -0,0 +1,27 @@
+id = int(input("Enter your id : "))
+
+print("Your id number: ", id)
+
+if id == 10:
+    print("Your name fahim.\nYour age 25.\nYour cgpa 3.00")
+elif id == 12:
+    print("Your name maruf")
+    print("Your age 27")
+    print("Your cgpa 3.00")
+
+elif id == 20 :
+    print("Your name pabel \n")
+    print("Your age 28")
+    print("Your cgpa 3.00")
+
+elif id == 15 :
+    print("Your name samad")
+    print("Your age 29")
+    print("Your cpga 3.50")
+elif id == 16 :
+    print("Your name sumon")
+    print("Your age 25")
+    print("Your cpga 3.20")
+
+else:
+    print("sorry is number not found")
